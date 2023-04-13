004fe4eea819415b25a8841c9304437dd246e7d7
Sopynu <hungcompo123@gmail.com>
2023-04-13 18:53:34 +0700

    the third commit

---
+++ src/exception.py
@@ -0,0 +1,26 @@
+import sys
+import logging
+
+def error_message_detail(error , error_detail:sys):
+    _ , _ , exec_tb = error_detail.exc_info()
+    file_name = exec_tb.tb_frame.f_code.co_filename
+    error_message = "Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+        file_name , exec_tb.tb_lineno , str(error)
+    )
+    return  error_message
+
+
+class CustomException(Exception):
+    def __init__(self, error_message , error_detail:sys ):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message  , error_detail)
+
+    def __str__(self):
+        return self.error_message
+
+if __name__ == "__main__":
+    try :
+        a = 1/0
+    except Exception as e:
+        logging.info("Divide By Zero")
+        raise CustomException(e , sys)
