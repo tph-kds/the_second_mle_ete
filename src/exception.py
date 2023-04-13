546e565136dca58c1be46f2429c5d951ad65306b
Sopynu <hungcompo123@gmail.com>
2023-04-13 23:41:24 +0700

    Ingestion Data

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error , error_detail:sys):
     _ , _ , exec_tb = error_detail.exc_info()
--- src/logger.py
+++ src/logger.py
@@ -14,5 +14,5 @@ logging.basicConfig(
     level=logging.INFO ,
 )
 
-if __name__ == "__main__":
-    logging.info("Logging has started")
+# if __name__ == "__main__":
+#     logging.info("Logging has started")
