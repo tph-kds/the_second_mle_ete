e09b9188c4190e531cd3571ee97e75f8e3dec93e
Sopynu <hungcompo123@gmail.com>
2023-04-14 19:15:08 +0700

    insert params into model"

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -42,13 +42,57 @@ class DataTrainer:
                 "Decision Tree" : DecisionTreeRegressor() , 
                 "Gradient Boosting" : GradientBoostingRegressor() , 
                 "Linear Regression" : LinearRegression() , 
-                "K-Neighbors Classifier" : KNeighborsRegressor() , 
-                "XGBClassifier" : XGBRegressor() , 
-                "CatBoosting Classifier" : CatBoostRegressor(verbose= False) ,
-                "AdaBoost Classifier" : AdaBoostRegressor() ,  
+                "K-Neighbors Regressor" : KNeighborsRegressor() , 
+                "XGBRegressor" : XGBRegressor() , 
+                "CatBoosting Regressor" : CatBoostRegressor(verbose= False) ,
+                "AdaBoost Regressor" : AdaBoostRegressor() ,  
             }
 
-            model_report:dict = evaluate_model(X_train = X_train  , y_train = y_train , X_test = X_test , y_test = y_test ,  models = models)
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "K-Neighbors Regressor" :{
+                    'n_neighbors' : [5 , 7 , 9 , 11] , 
+                    # 'weights' : ['uniform' ,'distance'] , 
+                    # 'algorithm': ['ball_tree' , 'kd_tree' , 'brute']
+                } , 
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+
+            model_report:dict = evaluate_model(X_train = X_train  , y_train = y_train , X_test = X_test , y_test = y_test 
+                                                ,  models = models , params = params)
             
 
             ## To get best model score from dict 
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 import dill
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -18,11 +19,17 @@ def save_objective(file_path , obj):
     except Exception as e:
         raise CustomException(e , sys)
 
-def evaluate_model(X_train , y_train , X_test , y_test , models):
+def evaluate_model(X_train , y_train , X_test , y_test , models ,params):
     try:
         report = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = params[list(models.keys())[i]]
+            
+            gs = GridSearchCV(model , para , cv = 3)
+            gs.fit(X_train , y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train , y_train)
 
             y_train_pred = model.predict(X_train)
